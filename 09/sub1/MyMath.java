7c0bf04b7b8e37fd4ecf3cfb42c47cb717987dc6
chouweixun <145731433+chouweixun@users.noreply.github.com>
2023-12-01 10:45:44 +0800

    Add files via upload

---
+++ 09/sub1/MyMath.java
@@ -0,0 +1,6 @@
+public abstract class MyMath {
+    public abstract void add(int a, int b);
+    public abstract void sub(int a, int b);
+    public abstract void mul(int a, int b) ;
+    public abstract void div(int a, int b) ;
+}

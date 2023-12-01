746e563312c6923112432acb4fedce3827a69111
chouweixun <145731433+chouweixun@users.noreply.github.com>
2023-12-01 10:24:42 +0800

    Add files via upload

---
+++ 04/Diag.java
@@ -0,0 +1,19 @@
+public class Diag {
+	private int width;
+    private int height;
+    
+    public Diag(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void show(){
+        System.out.println("w="+width+", H="+height);
+        System.out.printf("length=%5.2f\n",dia());
+    }
+
+    public double dia(){
+        return Math.sqrt(width*width+height*height);
+    }
+}
+
